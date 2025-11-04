7ec570f26d02d11ad6695477b2b91848f52be26c
matthewSheydaBCP <matthew.sheyda28@bcp.org>
2025-11-04 12:14:02 -0800

    sex update

--- game-alpha/main.py
+++ game-alpha/main.py
@@ -6,3 +6,12 @@ class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((settings.WINDOW_WIDTH, settings.WINDOW_HEIGHT))
+    def update(self):
+        pass
+    def new(self): #Create a new game and sprites.
+        self.p = Player(self, 6, 7)
+
+g = Game()
+
+while not pg.event.peek(pg.QUIT): #Checks if there is a quit event in events (User wants to exit).
+    g.update()
